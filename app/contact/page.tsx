2b3019e14bb43153039e5df69dd91d4a0f9fabd9
kris1027 <krzys89@gmail.com>
2026-04-27 22:51:05 +0200

    feat: build contact page and contact-items component

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,11 @@
-const ContactPage = () => {}
+import ContactItems from '@/components/contact/contact-items'
+
+const ContactPage = () => {
+  return (
+    <main className='p-4 space-y-4 max-w-7xl mx-auto'>
+      <ContactItems />
+    </main>
+  )
+}
 
 export default ContactPage
--- components/layout/navigation-links.tsx
+++ components/layout/navigation-links.tsx
@@ -5,8 +5,8 @@ const NavigationLinks = () => {
     <nav className='text-gray-300 text-sm p-4 space-x-4'>
       <Link href='/'>Strona Główna</Link>
       <Link href='/about'>O mnie</Link>
-      <Link href='/'>Usługi</Link>
-      <Link href='/'>Kontakt</Link>
+      <Link href='/services'>Usługi</Link>
+      <Link href='/contact'>Kontakt</Link>
     </nav>
   )
 }
